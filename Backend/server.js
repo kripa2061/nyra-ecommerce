2d187e8c8b46851726485bb441eba05bb51d68b4
Mamta-bot876 <bhattmamta001@gmail.com>
2026-02-16 18:21:32 +0545

    Added cart backend

--- Backend/server.js
+++ Backend/server.js
@@ -3,18 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 import connectDB from "./src/config/db.js";
-
 import productRouter from "./src/routes/productRouter.js";
+import cartRouter from "./src/routes/cartRoutes.js";
 import fileUpload from 'express-fileupload';
-
-
-
 import authRoutes from "./src/routes/authRoutes.js";
 import orderRouter from "./src/routes/orderRoutes.js";
 
 
 connectDB();
-
 const app = express();
 
 
@@ -22,14 +18,16 @@ app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({
     createParentPath: true
 }));
 app.use("/api/auth", authRoutes);
-app.use("/api/product",productRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/product", productRouter);
+app.use("/api/order", orderRouter);
+app.use("./api/cart", cartRouter);
 
 app.get("/", (req, res) => {
   res.send("API is running ");
